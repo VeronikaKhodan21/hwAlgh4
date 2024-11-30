dc5b975b6bda48d445381fb43aa50ef4daf3720f
VeronikaKhodan21 <146882332+VeronikaKhodan21@users.noreply.github.com>
2024-11-30 16:45:55 +0300

    work 1

--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -1,11 +1,14 @@
 package Tree;
 
+import Tree.Tree.Color;
+
 public class Tree {
     Node root;
     class Node{
         int value;
         Node left;
         Node right;
+        Color color;
     }
     public void insert(int value){
         if (root != null) {
@@ -15,6 +18,10 @@ public class Tree {
             root.value = value;
            }
         
+    }
+    enum Color{
+        BLACK,
+        RED
     }
     private void insert(Node node, int value){
         if(node.value != value){
@@ -31,6 +38,62 @@ public class Tree {
                         insert(node.left, value);}
             }
         }
+    public Node find(int value){
+        return find(root, value);
     }
+    private Node find(Node node, int value ){
+        if(node == null){
+            return null;
+        }if(node.value ==value){
+            return node;
+        }if(node.value < value){
+            return find(node.right, value);
+        }else{
+            return find(node.left , value);
+        }
+    }
+    private Node leftRotate(Node node){
+        Node cur = node.right;
+        node.right = cur.left;
+        cur.left = node;
+        cur.color = node.color;
+        node.color= Color.RED;
+        return cur;
+    }
+    private Node rightRotate(Node node){
+        Node cur = node.left;
+        node.left = cur.right;
+        cur.right = node;
+        cur.color = node.color;
+        node.color= Color.RED;
+        return cur;
+    }
+    private void swspColor(Node node){
+        node.color = (node.color == Color.RED ? Color.BLACK : Color.RED);
+        node.left.color = Color.BLACK;
+        node.right.color = Color.BLACK;
+    }
+    private Node balanc(Node node){
+        boolean flag = true;
+        Node cur = node;
+        do{
+            flag = false;
+
+            if(cur.right != null && cur.right.color == Color.RED && (cur.left == null || cur.left.color == Color.BLACK)){
+                cur = leftRotate(cur);
+                flag = true;
+            }
+            if(cur.left != null && cur.left.color == Color.RED && cur.left.left != null && cur.left.left.color == Color.RED){
+                cur = rightRotate(cur);
+                flag = true;
+            }
+            if(cur.left != null && cur.left.color == Color.RED && cur.right != null && cur.right.color == Color.RED){
+                swspColor(cur);
+                flag = true;
+            }
+        }while (flag);
+        return  cur;
+    }
+}
 
 
